3b3f3684d0cdb1f366089c271340489e10e7ca77
Nguyen Trong Hieu <hieuntctk42@gmail.com>
2022-05-03 10:53:00 +0700

    add author label

--- src/views/client/blog_detail.html.php
+++ src/views/client/blog_detail.html.php
@@ -138,7 +138,15 @@
                     <div class="media g-mb-30 media-comment">
                         <div class="media-body u-shadow-v18 g-bg-secondary g-pa-30">
                             <div class="g-mb-15">
-                                <h5 class="h5 g-color-gray-dark-v1 mb-0"><?= $comment->last_name . ' ' . $comment->first_name ?></h5>
+                                <div class="d-flex align-items-baseline">
+                                    <h5 class="h5 g-color-gray-dark-v1 mb-0">
+                                        <?= $comment->last_name . ' ' . $comment->first_name ?>
+                                    </h5>
+                                    <?php if ($comment->author_id): ?>
+                                        <span class="text-muted g-font-size-12 d-block ms-1">Tác giả</span>
+                                    <?php endif; ?>
+                                </div>
+
                                 <span
                                     class="g-color-gray-dark-v4 g-font-size-12"><?= $comment->get_created_at()->format('d-m-Y H:i:s') ?></span>
                             </div>
